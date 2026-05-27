4b9f1004f78ce8c81b3967dad234ffce04014cd6
SpeedyPL <dawid.gogolin.lcfc@gmail.com>
2026-05-27 16:35:39 +0200

    1.21 update

--- common/src/main/java/net/zebatek/simple_woodcutter/compat/WoodcuttingCategory.java
+++ common/src/main/java/net/zebatek/simple_woodcutter/compat/WoodcuttingCategory.java
@@ -1,46 +1,68 @@
 package net.zebatek.simple_woodcutter.compat;
 
+import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
-import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
-import mezz.jei.api.recipe.category.AbstractRecipeCategory;
+import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.zebatek.simple_woodcutter.SimpleWoodcutter;
 import net.zebatek.simple_woodcutter.block.ModBlocks;
 import net.zebatek.simple_woodcutter.recipe.WoodcutterRecipe;
 
-public class WoodcuttingCategory extends AbstractRecipeCategory<WoodcutterRecipe> {
+public class WoodcuttingCategory implements IRecipeCategory<WoodcutterRecipe> {
 
     public static final ResourceLocation UID = ResourceLocation.fromNamespaceAndPath(SimpleWoodcutter.MOD_ID, "woodcutting");
     public static final RecipeType<WoodcutterRecipe> WOODCUTTING_TYPE = new RecipeType<>(UID, WoodcutterRecipe.class);
 
+    private final IDrawable background;
+    private final IDrawable icon;
+
     public WoodcuttingCategory(IGuiHelper helper) {
-        super(
-                WOODCUTTING_TYPE,
-                Component.translatable("container.simple_woodcutter.woodcutter"),
-                helper.createDrawableItemLike(ModBlocks.WOODCUTTER),
-                82,
-                34
-        );
+        ResourceLocation jeiVanillaGui = ResourceLocation.fromNamespaceAndPath("jei", "textures/jei/gui/gui_vanilla.png");
+        this.background = helper.createDrawable(jeiVanillaGui, 0, 220, 82, 34);
+        this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(ModBlocks.WOODCUTTER_ITEM));
+    }
+
+    @Override
+    public RecipeType<WoodcutterRecipe> getRecipeType() {
+        return WOODCUTTING_TYPE;
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.translatable("container.simple_woodcutter.woodcutter");
+    }
+
+    @Override
+    public IDrawable getBackground() {
+        return this.background;
+    }
+
+    @Override
+    public IDrawable getIcon() {
+        return this.icon;
     }
 
     @Override
     public void setRecipe(IRecipeLayoutBuilder builder, WoodcutterRecipe recipe, IFocusGroup focuses) {
-        builder.addInputSlot(1, 9)
-                .setStandardSlotBackground()
+        builder.addSlot(RecipeIngredientRole.INPUT, 1, 9)
                 .addIngredients(recipe.getIngredients().get(0));
 
-        builder.addOutputSlot(61, 9)
-                .setOutputSlotBackground()
-                .addItemStack(recipe.getResultItem(Minecraft.getInstance().level.registryAccess()));
+        Minecraft minecraft = Minecraft.getInstance();
+        builder.addSlot(RecipeIngredientRole.OUTPUT, 61, 9)
+                .addItemStack(recipe.getResultItem(minecraft.level.registryAccess()));
     }
 
     @Override
-    public void createRecipeExtras(IRecipeExtrasBuilder builder, WoodcutterRecipe recipe, IFocusGroup focuses) {
-        builder.addRecipeArrow().setPosition(26, 9);
+    public void draw(WoodcutterRecipe recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
     }
 }
